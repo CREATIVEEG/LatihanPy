062f58c4e5853c86ecfdc80514d64543fe444677
Rhendydiki <rhendydikin@gmail.com>
2022-10-25 17:15:40 +0700

    Penambahan file python

---
+++ Latihan2.py
@@ -0,0 +1,12 @@
+#Menjumlahkan bilangan dengan variabel a & b
+
+#Mendefinisikan variabel a & b
+a = 8
+b = 6
+
+#Mencetak nilai variabel a & b
+print("Variabel a =",a)
+print("Variabel b =",b)
+
+#Mencetak hasil penjumlahan a & b
+print("Hasil penjumlahan a & b =",a+b)
